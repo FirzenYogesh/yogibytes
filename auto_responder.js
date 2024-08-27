465a4a8e14e265b900f2dc33540c44790bf260bf
Yogesh S <firzenyogesh@gmail.com>
2024-08-28 00:04:12 +0530

    add timeout for each platform

--- auto_responder.js
+++ auto_responder.js
@@ -1,32 +1,32 @@
 const patterns = [
     {
         pattern: /((when|ep?po|bro)(.*))?((play|join)(.*)((with|wth)(.*)(yo)?u(r)?|together|me|(sub(scriber)?s?))(game)?)|((party|team|squad)(.*)(up)?|game|(sub(scriber)?s?)|(v)?(onna)|(ung(a|o)l(a|o)da)|(k(u|o)o?da)|(se(r)?n(d|t)hu)|join|come|nam(m|b)a)(.*)(play|a(a)?d(a|u)|pan(n)?(a|u)|session|(n(e)?xt(.*)match)|v(e|a)l(a|e)d(a|u)|vara)(.*)/ig,
-        lastSentAt: 0,
+        lastSentAt: {},
         message: "You Can play with YogiBytes on the Team Up Sessions, follow YogiBytes Socials for the announcement!",
     },
     {
         pattern: /(when|what|ep(p)?(a|o)|en(d|t)ha)(.*)(start(ed)?|a(a)?da|pann|play)(.*)/ig,
-        lastSentAt: 0,
+        lastSentAt: {},
         message: "YogiBytes Started Playing Fortnite From Chapter 1 Season 2 (2018)!",
     },
     {
         pattern: /((unga|you)(.*))?((what|en(n)?a)(.*))?(pc|(c|g)pu|processor|(graphics card))(.*)((spec|specification|component|config|build|cost|price|ev(a|o)l(a|o)|enna)(s|uration)?(.*))?(enna|(a)?achu|have|vachir)?(.*)/ig,
-        lastSentAt: 0,
+        lastSentAt: {},
         message: "YogiBytes PC Specs: Processor: Intel Core i7 14700K, RAM: 32GB DDR5, GPU: Zotac GeForce 4080 Super. Full Specs and Price Video: https://youtu.be/ld4alEIMCwo",
     },
     {
         pattern: /(fav(e)?(o(u)?rite)?|p(u|i)d(u|i)cha)(\s)?(game)/ig,
-        lastSentAt: 0,
+        lastSentAt: {},
         message: "YogiBytes Favorite Games are: God of War, Pokemon, Assassin's Creed, Mario, Red Dead Redemption, Batman Arkham, GTA V, GTA San Andreas, Fortnite",
     },
     {
         pattern: /((en?(n|g)a|wh(at|ere)|wer|do(.*)(yo)?u)(.*)(velai|job|work|do(.*)(for)?(living)))|((work(ing)?|job|velai)(.*)o?r(.*))|((full|part)(.*)(time(.*))?(stream)(.*))/ig,
-        lastSentAt: 0,
+        lastSentAt: {},
         message: "YogiBytes is a Software Engineer, that's his Primary Job. YouTubing is one of his hobby!"
     },
     {
         pattern: /((en?n(t|d)?h?a)(.*)|wh(at|ere|ich)|wer|do(.*)(yo)?u)(.*)(ooru?|city|stay|live|loca|country)(.*)/ig,
-        lastSentAt: 0,
+        lastSentAt: {},
         message: "YogiBytes is from Madurai, he works in Chennai!"
     }
 ]
@@ -43,8 +43,12 @@ function applyCustomActions(data){
             let chatmessage = data.chatmessage;
             for (let i = 0; i < patterns.length; i++) {
                 let item = patterns[i];
-                if(item.pattern.test(chatmessage) && Date.now() - item.lastSentAt > globalTimeout) {
-		            item.lastSentAt = Date.now();
+                let lastSentAt = item.lastSentAt[data.type];
+                if (!lastSentAt) {
+                    lastSentAt = 0;
+                }
+                if(item.pattern.test(chatmessage) && Date.now() - lastSentAt > globalTimeout) {
+                    item.lastSentAt[data.type] = Date.now()
                     respondP2P(item.message, tid);
                 }
             }
